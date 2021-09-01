6eda00b1a783adb6b668a1bca19611ecc156e207
Christopher <christophergerardy778@gmail.com>
2021-09-01 13:05:39 -0500

    trazar ruta con fallos

--- app/src/main/java/com/example/inride/ui/HomeActivity.kt
+++ app/src/main/java/com/example/inride/ui/HomeActivity.kt
@@ -10,6 +10,11 @@ import com.mapbox.android.core.location.LocationEngineCallback
 import com.mapbox.android.core.location.LocationEngineProvider
 import com.mapbox.android.core.location.LocationEngineRequest
 import com.mapbox.android.core.location.LocationEngineResult
+import com.mapbox.api.directions.v5.DirectionsCriteria
+import com.mapbox.api.directions.v5.MapboxDirections
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
@@ -20,6 +25,10 @@ import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeActivity : AppCompatActivity(), OnMapReadyCallback,
     LocationEngineCallback<LocationEngineResult> {
@@ -28,6 +37,7 @@ class HomeActivity : AppCompatActivity(), OnMapReadyCallback,
     private val intervalInMilliseconds = 1000L
     private val maxWaitTime = intervalInMilliseconds * 5
 
+    private lateinit var navigationRoute: NavigationMapRoute
     private lateinit var mapBoxMap: MapboxMap
     private lateinit var mapView: MapView
 
@@ -99,8 +109,56 @@ class HomeActivity : AppCompatActivity(), OnMapReadyCallback,
                     .build()
 
                 mapBoxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+
+                getRoute(p0.lastLocation!!.latitude, p0.lastLocation!!.longitude)
             }
         }
+    }
+
+    private fun getRoute(lat: Double, lon: Double) {
+        val client = MapboxDirections.builder()
+            .origin(Point.fromLngLat(lat, lon))
+            .destination(Point.fromLngLat(lat -1, lon -1))
+            .steps(true)
+            .accessToken(getString(R.string.mapbox_token))
+            .overview(DirectionsCriteria.OVERVIEW_FULL)
+            .profile(DirectionsCriteria.PROFILE_DRIVING)
+            .build()
+
+        client.enqueueCall(object : Callback<DirectionsResponse> {
+            override fun onResponse(
+                call: Call<DirectionsResponse>,
+                response: Response<DirectionsResponse>
+            ) {
+                if (response.body() == null) {
+                    Log.d("magia", "error general")
+                    return
+                }
+                else if (response.body()!!.routes().size < 1) {
+                    Log.d("magia", "no hay rutas")
+                    return
+                }
+
+                val route = response.body()!!.routes()[0]
+
+                if (::navigationRoute.isInitialized) {
+                    navigationRoute.removeRoute()
+                } else {
+                    navigationRoute = NavigationMapRoute(null, mapView, mapBoxMap, R.style.NavigationMapRoute)
+                }
+
+                if (route != null) {
+                    Log.d("suc", "success")
+                    navigationRoute.addRoute(route)
+                }
+
+            }
+
+            override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
+                Log.d("maperror", "error")
+            }
+
+        })
     }
 
     override fun onFailure(p0: Exception) {
